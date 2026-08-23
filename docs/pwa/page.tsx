8e4ad5fb364b4d3c52c65490e5479076485b8e86
Nuno <nuno.80.al@gmail.com>
2026-08-23 11:57:19 +0200

    pulizia files

--- docs/pwa/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function OfflinePage() {
-  return (
-    <main style={{ padding: "2rem", textAlign: "center" }}>
-      <h1>Sei offline</h1>
-      <p>Controlla la connessione e riprova.</p>
-    </main>
-  );
-}
